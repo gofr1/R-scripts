67222ea78d73407b6cccbdc93281350cc2b22313
Ilia Odintsov <Ilia_Odintsov@epam.com>
2020-03-04 16:01:56 +0100

    Add power function plus some fixes

--- math.R
+++ math.R
@@ -1,3 +1,4 @@
+# Factorial calculation with recursion
 factorialCalc <- function(x) {
     if (x < 0) {
        "Must be positive integer"
@@ -8,6 +9,12 @@ factorialCalc <- function(x) {
     }
 }
 
+print(factorialCalc(5)) # 120
+print(factorialCalc(10)) # 3628800
+print(factorialCalc(-1)) # Must be positive integer
+print(factorialCalc(0)) # 1
+
+# Calc first x Fibonacci numbers
 retFibonacci <- function(x) {
    if (x < 1) {
       "Must be positive integer not equal to zero or one"
@@ -26,7 +33,50 @@ retFibonacci <- function(x) {
 
 retFibonacci(10)
 
-print(factorialCalc(5)) # 120
-print(factorialCalc(10)) # 3628800
-print(factorialCalc(-1)) # Must be positive integer
-print(factorialCalc(0)) # 1
+# Factorial calculation with For loop
+factorialLoopCalc <- function(x) {
+    if (x < 0) {
+       "Must be positive integer"
+    } else if (x == 0 | x == 1) {
+       1
+    } else {
+       for (z in 1:(x-1)) {
+          x <- z * x
+       }
+       x
+    }
+}
+
+print(factorialLoopCalc(5)) # 120
+print(factorialLoopCalc(10)) # 3628800
+print(factorialLoopCalc(-1)) # Must be positive integer
+print(factorialLoopCalc(0)) # 1
+print(factorialLoopCalc(1))
+print(factorialLoopCalc(2))
+
+# power function
+calcPower <- function(x, y) {
+   z <- 0
+   if (x == 0) {
+      0
+   } else if (y == 0) {
+      1
+   } else {
+      while (y != sign(y)) {
+         x = x * x 
+         y <- y - sign(y)
+      }
+      if (sign(y) > 0) {
+         x
+      } else {
+         1/x
+      }
+   }
+}
+
+calcPower(2, 2) # 4
+calcPower(1, 4) # 1
+calcPower(10, 1) # 10
+calcPower(10, -1) # 0.1
+calcPower(10, 2) # 100
+calcPower(10, -2) # 0.01
