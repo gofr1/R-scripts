1501b62c77d50db62be13bdc5d77a7facc5d9794
Ilia Odintsov <gofr.one@gmail.com>
2020-03-13 20:17:14 +0100

    Keras and neuralet examples

--- packagesSearch.R
+++ packagesSearch.R
@@ -1,7 +1,5 @@
 as.data.frame(installed.packages()[,c(3:4)])
 
-install.packages("ggplot2")
-install.packages("RODBC")
 install.packages("psych")
 install.packages("readr")
 install.packages("data.table")
@@ -10,5 +8,18 @@ install.packages("caret")
 install.packages("rpart.plot")
 install.packages("corrplot")
 install.packages("e1071")
-install.packages("microbenchmark")
 install.packages("randomForest")
+
+# graphics
+install.packages("ggplot2")
+install.packages("RColorBrewer")
+
+# benchmarking
+install.packages("microbenchmark")
+
+# databases
+install.packages("RODBC")
+
+# neural networks
+install.packages("keras")
+install.packages("neuralnet")
